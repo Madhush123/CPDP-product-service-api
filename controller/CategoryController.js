a2478a47d8fa8884cd41066016fa43d3984f7bd2
Madhushan <madhushan999@gmail.com>
2025-06-25 22:34:37 +0530

    Initial commit

---
+++ controller/CategoryController.js
@@ -0,0 +1,35 @@
+const CategorySchema=require('../model/CategorySchema');
+const {request, response} = require("express");
+
+//save(POST)
+const createCategory= (request,response)=>{
+    console.log(request.body);
+}
+
+//update(PUT)
+const updateCategory= (request,response)=>{
+    console.log(request.body);
+}
+
+//delete(DELETE)
+const deleteCategory= (request,response)=>{
+    console.log(request.body);
+}
+
+//find by id(GET)
+const findCategoryById= (request,response)=>{
+    console.log(request.body);
+}
+
+//find all(GET)
+const findAllCategories= (request,response)=>{
+    console.log(request.body);
+}
+
+module.exports={
+    createCategory,
+    updateCategory,
+    deleteCategory,
+    findCategoryById,
+    findAllCategories
+}
